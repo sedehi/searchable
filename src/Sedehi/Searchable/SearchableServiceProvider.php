bfcfaea9589f39a12e9d9ef512989b7e25ddb524
Navid Sedehi <navid.sedehi@gmail.com>
2015-09-03 20:08:35 +0430

    Update SearchableServiceProvider.php

--- src/Sedehi/Searchable/SearchableServiceProvider.php
+++ src/Sedehi/Searchable/SearchableServiceProvider.php
@@ -20,14 +20,12 @@ class SearchableServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->publishes([__DIR__.'/../../config/searchable.php' => config_path('searchable.php')]);
-
+        $this->package('sedehi/searchable');
     }
 
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../../config/searchable.php', 'searchable');
     }
 
     /**
