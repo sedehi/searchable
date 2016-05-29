495e6fbf3f919fca4d77f37a7650e830e94e5a42
navid <navid.sedehi@gmail.com>
2016-05-29 20:58:25 +0430

    add date format

--- src/Sedehi/Searchable/Searchable.php
+++ src/Sedehi/Searchable/Searchable.php
@@ -1,15 +1,15 @@
 <?php namespace Sedehi\Searchable;
 
+use ClassPreloader\Config;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Config;
 
 trait Searchable
 {
 
     public function scopeSearchable($query)
     {
-        $dates = array_unique(array_merge(Config::get('searchable.date_fields'), $this->dates));
+        $dates = array_unique(array_merge(config('searchable.date_fields'), $this->dates));
 
 
         foreach ($this->searchable as $key => $value) {
@@ -68,37 +68,38 @@ trait Searchable
 
     private function mktime()
     {
-        if (Config::get('searchable.date_type') === 'gregorian') {
+        if (config('searchable.date_type') === 'gregorian') {
             return 'mktime';
-        }else{
-			if(!function_exists('jmktime')){
-				throw new \Exception('jmktime functions are available');
-			}
-			return 'jmktime';
-		}
-		
-		
+        } else {
+            if (!function_exists('jmktime')) {
+                throw new \Exception('jmktime functions are unavailable');
+            }
 
-
-        
+            return 'jmktime';
+        }
     }
 
     private function convertDate($date)
     {
 
         $mktimeFunction = $this->mktime();
-        $dateTime       = array();
+        $dateTime       = [];
         $dateTime[3]    = 0;
         $dateTime[4]    = 0;
         $dateTime[5]    = 0;
-        $dateTime       = array_merge(explode(Config::get('searchable.date_divider'), $date), $dateTime);
+        $dateTime       = array_merge(explode(config('searchable.date_divider'), $date), $dateTime);
+        $formats        = ['d' => 0, 'm' => 1, 'y' => 2, 'h' => 3, 'i' => 4, 's' => 5];
 
         if (count($dateTime) == 6) {
-            $timestamp = $mktimeFunction($dateTime[3], $dateTime[4], $dateTime[5], $dateTime[1], $dateTime[0],
-                                         $dateTime[2]);
+            if (!is_null(config('searchable.date_format'))) {
+                $formats = array_flip(explode(config('searchable.date_divider'), config('searchable.date_format')));
+            }
+            $timestamp = $mktimeFunction($dateTime[$formats['h']], $dateTime[$formats['i']], $dateTime[$formats['s']],
+                                         $dateTime[$formats['m']], $dateTime[$formats['d']], $dateTime[$formats['y']]);
 
             return date($this->getDateFormat(), $timestamp);
         }
+
 
         return false;
     }
@@ -114,5 +115,4 @@ trait Searchable
                 break;
         }
     }
-
 }
--- src/config/searchable.php
+++ src/config/searchable.php
@@ -4,12 +4,13 @@ return [
 
 
     //gregorian or jalali
-    'date_type'     => 'jalali',
-    'date_fields'   => [
+    'date_type'    => 'jalali',
+    'date_fields'  => [
         'created_at',
         'updated_at',
         'deleted_at'
     ],
-    'date_divider'  => '-',
+    'date_divider' => '-',
+    'date_format'  => 'y-m-d-h-i-s',
 
 ];
